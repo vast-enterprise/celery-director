bbfb06e77b6429f04ea828266e10b69a1aac0a6b
TotallyNewGuy <haocharles@outlook.com>
2025-03-07 13:55:49 +0800

    cancel validate

--- director/extensions.py
+++ director/extensions.py
@@ -42,7 +42,9 @@ def validate_tasks(task_definition, tasks_config):
         if isinstance(task, str):
             # 无条件的任务
             if task not in tasks_config:
-                raise WorkflowSyntaxError(f"task '{task}' is not found in {config.TASKS_CONFIG_PATH}")
+                pass
+                # 检查 task_name 被取消
+                # raise WorkflowSyntaxError(f"task '{task}' is not found in {config.TASKS_CONFIG_PATH}")
         elif isinstance(task, list):
             validate_tasks(task, tasks_config)
         else: # dict
