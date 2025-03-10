a9dd7a05adbc97a8aab08e34b2803214172f6d26
liangd <liangding1990@163.com>
2025-03-10 11:39:29 +0800

    add expire for task apply_async

--- director/builder.py
+++ director/builder.py
@@ -240,6 +240,7 @@ class WorkflowBuilder(object):
                 link=self.success_hook_canvas,
                 # 失败的 hook 会在 celery beat 执行
                 link_error=self.failure_hook_canvas,
+                expire=3600 # TODO: 应该放在设置里
             )
 
         except Exception as e:
