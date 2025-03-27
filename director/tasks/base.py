a6381a1c311ce48052340a07bbccf2b7880e425e
liangd <liangding1990@163.com>
2025-03-27 08:53:14 +0800

    remove check max_retry

--- director/tasks/base.py
+++ director/tasks/base.py
@@ -7,9 +7,6 @@ from celery.utils.log import get_task_logger
 from director.extensions import cel, db
 from director.models import StatusType
 from director.models.tasks import Task
-config_path = Path(os.getenv("DIRECTOR_CONFIG")).resolve()
-sys.path.append(f"{config_path.parent.resolve()}/")
-import config
 
 logger = get_task_logger(__name__)
 
@@ -36,14 +33,13 @@ def close_session(*args, **kwargs):
 
 class BaseTask(_Task):
     def on_failure(self, exc, task_id, args, kwargs, einfo):
-        if self.request.retries >= config.MAX_RETRY:
-            task = Task.query.filter_by(id=task_id).first()
-            task.status = StatusType.error
-            task.result = {"exception": str(exc), "traceback": einfo.traceback}
-            task.workflow.status = StatusType.error
-            task.save()
+        task = Task.query.filter_by(id=task_id).first()
+        task.status = StatusType.error
+        task.result = {"exception": str(exc), "traceback": einfo.traceback}
+        task.workflow.status = StatusType.error
+        task.save()
 
-            logger.info(f"Task {task_id} is now in error")
+        logger.info(f"Task {task_id} is now in error")
         super(BaseTask, self).on_failure(exc, task_id, args, kwargs, einfo)
 
     def on_success(self, retval, task_id, args, kwargs):
